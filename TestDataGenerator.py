69cfee5783aeafd7567e27499e711d79a4a92091
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-07-17 21:31:24 +0200

    Generate test data out of season

--- TestDataGenerator.py
+++ TestDataGenerator.py
@@ -9,12 +9,40 @@ class TestDataGenerator(object):
         data = []
         for i in range(5, 33):
             game_day = self.api.get_game_day(league, season, i)
-            game_day_data = self.genererateFromGameDay(game_day)
+            game_table = self.api.get_game_table(league, season, i)
+            game_day_data = self.genererateFromGameDay(game_day, game_table)
             data.extend(game_day_data)
         return data
 
-    def genererateFromGameDay(self, game_day):
+    def genererateFromGameDay(self, game_day, game_table):
         game_day_data = []
-        for x in game_day:
-            game_day_data.append(([],[]))
+        for game in game_day:
+            ht = game.get_home_team()
+            hp = game.get_home_points()
+            pos_home = game_table.get_position(ht)
+            x_pos_home = self.get_input_for_position(pos_home)
+            at = game.get_away_team()
+            ap = game.get_away_points()
+            pos_away = game_table.get_position(at)
+            x_pos_away = self.get_input_for_position(pos_away)
+
+
+            y_points_home = self.get_output_for_points(hp, ap)
+            y_points_away = self.get_output_for_points(ap, hp)
+
+            result = self.calculate_result(hp, ap)
+            game_day_data.append(([x_pos_home, x_pos_away],[y_points_home, y_points_away],[result]))
         return game_day_data
+
+    def get_input_for_position(self, position):
+        return max(((18 - position) / 17), 0.01)
+
+    def get_output_for_points(self, x, y):
+        return 0.99 if x > y else 0.01
+
+    def calculate_result(self, home, away):
+        if home > away:
+            return 1
+        if away > home:
+            return 2
+        return 0
--- TestDataGeneratorTest.py
+++ TestDataGeneratorTest.py
@@ -1,5 +1,8 @@
 import unittest
 from TestDataGenerator import TestDataGenerator
+import numpy
+import pandas
+import matplotlib.pyplot as plt
 
 
 class GenerateData(unittest.TestCase):
@@ -9,6 +12,40 @@ class GenerateData(unittest.TestCase):
 
         self.assertTrue(len(data) > 100)
         self.assertTrue(len(data) < 1000)
+
+        for d in data:
+            (input, output, result) = d
+            self.assertTrue(len(input) == 2)
+            self.assertTrue(len(output) == 2)
+            self.assertTrue(len(result) == 1)
+
+            self.assertTrue(input[0] > 0)
+            self.assertTrue(input[0] <= 1)
+
+            self.assertTrue(output[0] > 0)
+            self.assertTrue(output[0] < 1)
+
+            self.assertTrue(result[0] in [0,1,2])
+
+    def _test_season_explore(self):
+        gen = TestDataGenerator()
+        data = gen.generateFromSeason('bl1', '2014')
+
+        print data
+
+        l = len(data)
+        x_train = numpy.zeros((l, 2), int)
+        y = numpy.zeros((l,1), int)
+
+        for i in range(0, l):
+            (a , b) = data[i]
+            y[i] = b[0]
+            x_train[i][0] = a[0]
+            x_train[i][1] = a[1]
+
+        dataframe = pandas.DataFrame(x_train, columns=['home','away'])
+        pandas.plotting.scatter_matrix(dataframe, c=y, figsize=(20, 20), s=1000)
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
