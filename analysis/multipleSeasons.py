f7d3d329e99c5c3d53b16f1652411a575c16aced
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-07-31 21:57:26 +0200

    Add analysis for multiple seasons

---
+++ analysis/multipleSeasons.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+from NetTrainer import NetTrainer
+from NeuralNetwork import NN2
+
+x = []
+y = []
+alpha = 0.1
+net = NN2(6, 10, 2, alpha)
+trainer = NetTrainer(net)
+
+i = 0
+for r in range(0,10):
+    for season in ['2011', '2012', '2013', '2014', '2015']:
+        trainer.train_season('bl1', season)
+        (result, _, _, _) = trainer.check_season('bl1', '2016')
+        i = i + 1
+        x.append(i)
+        y.append(result)
+
+        trainer.train_season('bl2', season)
+        (result, _, _, _) = trainer.check_season('bl1', '2016')
+        i = i + 1
+        x.append(i)
+        y.append(result)
+
+print 'Result', max(y)
+
+plt.plot(x, y, 'ro')
+plt.axis([0, i, 50, 100])
+plt.show()
