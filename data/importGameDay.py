26a00229565186db0d5ca69320f264a6661d3988
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-08-21 20:51:01 +0200

    Add first game days of 2017

--- data/importGameDay.py
+++ data/importGameDay.py
@@ -2,8 +2,8 @@ import SQLiteAPI
 
 league = 'bl2'
 season = '2017'
-game_day = 1
+game_day = 3
 
 
 api = SQLiteAPI.SQLiteAPI()
-api.import_game_day(league, season, game_day, True)
+api.import_game_day(league, season, game_day, False)
