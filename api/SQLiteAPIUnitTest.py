f415c02c742fb919b1d2e7309b753732185174a7
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-12-26 16:34:50 +0100

    Add some more unit tests

--- api/SQLiteAPIUnitTest.py
+++ api/SQLiteAPIUnitTest.py
@@ -8,10 +8,13 @@ class SQLiteDataUnitTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(SQLiteDataUnitTest, self).__init__(*args, **kwargs)
         self.api = None
+        self.cursor = None
 
     def setUp(self):
         self.api = SQLiteAPI.SQLiteAPI()
         self.api.conn = MagicMock()
+        self.cursor = MagicMock()
+        self.api.conn.cursor.return_value = self.cursor
 
     def _create_result_data(self):
         result = {}
@@ -31,8 +34,35 @@ class SQLiteDataUnitTest(unittest.TestCase):
     def test_import_data_empty(self):
         data = []
         self.api._import_data('league', 'season', data)
+        self.assertEquals(self.cursor.execute.call_count, 0)
 
     def test_import_data_one_game(self):
         game = self._create_game_data()
         data = [game]
         self.api._import_data('league', 'season', data)
+        self.assertEquals(self.cursor.execute.call_count, 2)
+
+    def test_import_data_two_games(self):
+        game1 = self._create_game_data(match_id=1)
+        game2 = self._create_game_data(match_id=2)
+        data = [game1, game2]
+        self.api._import_data('league', 'season', data)
+        self.assertEquals(self.cursor.execute.call_count, 4)
+
+    def test_import_data_debug(self):
+        game = self._create_game_data()
+        data = [game]
+        self.api._import_data('league', 'season', data, debug=True)
+        self.assertEquals(self.cursor.execute.call_count, 0)
+
+    def test_points_home(self):
+        points = self.api.calculate_points(2, 1)
+        self.assertEquals(points, 3)
+
+    def test_points_away(self):
+        points = self.api.calculate_points(1, 5)
+        self.assertEquals(points, 0)
+
+    def test_points_deuce(self):
+        points = self.api.calculate_points(0, 0)
+        self.assertEquals(points, 1)
