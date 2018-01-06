b7ecaeccb988219c06b8e0861bf73fd0a3fff8c5
Ralf Pannemans <ralf.pannemans@googlemail.com>
2018-01-06 11:43:08 +0200

    Add another net

--- analysis/compare_nets.py
+++ analysis/compare_nets.py
@@ -1,13 +1,18 @@
 import os
+import logging
+from analysis.Util import init_logging
 from prediction.Benchmark import load_and_check
 
 NET_PATH = "./prediction/pickles"
 
 def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     for data_file in os.listdir(NET_PATH):
         if data_file.endswith(".pickles"):
             filename = os.path.join(NET_PATH, data_file)
             load_and_check(filename)
 
 if __name__ == '__main__':
+    init_logging()
     main()
